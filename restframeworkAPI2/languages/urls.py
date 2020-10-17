b365f8d1da5b38e12e362e6d5fb97a609a935927
Pradeep Kumar <67951038+rpradeep02@users.noreply.github.com>
2020-10-17 11:01:20 +0530

    Add files via upload

---
+++ restframeworkAPI2/languages/urls.py
@@ -0,0 +1,14 @@
+#from django.contrib import admin
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('language', views.LanguageView)
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+
+]
+
